b15c7554b8f641eed36907e838488d4d0d862c45
Мухаммед <muhammadsodikmavlanov27@gmail.com>
2024-09-03 16:02:57 +0600

    First commit

---
+++ src/Phone.java
@@ -0,0 +1,18 @@
+public class Phone {
+    private String model;
+    private int storage;
+
+    public Phone(String model, int storage) {
+        this.model = model;
+        this.storage = storage;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getStorage() {
+        return storage;
+    }
+}
+
